4d6523a6081b2df9129b4f8cc8e4ab2302b5cee0
G-Miralles <g.miralles@itecriocuarto.org.ar>
2021-02-25 15:55:48 -0300

    the routes post and put methods were added

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,3 +1,9 @@
+const bodyParser = require('body-parser');
+
 module.exports = app =>{
     app.set('port', process.env.PORT || 3000);
+
+    app.use(bodyParser.json());                         //voy a poder entender los datos que me mandan via psot
+    app.use(bodyParser.urlencoded({extended: false}));   //Me permite entender los datos que me mandan via rul
+
 };
--- routes/movies.js
+++ routes/movies.js
@@ -1,14 +1,41 @@
+const { response } = require('express');
 const mongojs = require('mongojs');
 const db = mongojs('DatabaseMovies', [
     'movies'
 ]);
 
 module.exports = app => {
+
+
     app.get('/movies', (req, res) => {
         db.movies.find((err, movies) => {
             res.json({
-                response: movies
+                movies
             });
         });
     });
+
+    app.post('/movies', (req, res) => {
+
+        let newMovie = req.body;
+        db.movies.insert(newMovie, (err, movie) => {
+            res.json({
+                movie  // esto es igual a movie: movie
+            });
+        })
+    });
+
+    app.put('/movies/:id', (req, res) => {
+        let updatedMovie = req.body;
+        db.movies.update(
+            {_id: mongojs.ObjectID(req.params.id)},
+            { $set: { updatedMovie} },
+            { upsert: true },
+            (err,response) => {
+                res.json({
+                    response
+                })
+            }
+        )
+    })
 };
